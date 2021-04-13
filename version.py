73c061e5880f013f4637d9fbcdd422ea0cfb174e
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2021-04-13 10:58:03 +0200

    Adapt version.py to new versioning scheme.

--- version.py
+++ version.py
@@ -2,8 +2,9 @@
 # Distributed under the terms of the Modified BSD License.
 
 version_info = (
-    0,
-    0,
+    2021,
+    4,
+    13,
     1
 )
 __version__ = '.'.join(map(str, version_info[:3]))
